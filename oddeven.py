31f70cdc049e688445545adc4d193fb87be759b1
Brahmdeep Singh <79687339+brahmdeep3541@users.noreply.github.com>
2022-07-27 22:14:55 +0530

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,8 @@
+print("Hello 😊😊")
+num = int(input("please enter the number: "))
+
+val = num % 2
+if val == 0:
+    print("The number provided is even !!")
+else :
+    print("The number provided is odd !!")
